8cf449b3f762e66157a107e215022d47d2397cd5
Бабкин Алексей <babkin.92@mail.ru>
2023-11-15 02:29:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weightInKg = 98;
+        double heightInM = 1.87;
+        double bmi = service.bmi(98,1.87);
+        System.out.println(bmi);
+    }
+}
