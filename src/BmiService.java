a947d6c382ff9307e972c50a34f30549f214d88b
Бабкин Алексей <babkin.92@mail.ru>
2023-11-15 20:56:57 +0300

    Initial commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double bmi( double weightInKg, double heightInM){
+    public double bmi(double weightInKg, double heightInM) {
         double bmi = weightInKg / (heightInM * heightInM);
-    return (int) bmi;
+        return (int) bmi;
     }
 }
